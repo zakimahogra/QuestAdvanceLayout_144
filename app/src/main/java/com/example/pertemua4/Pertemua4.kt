19a39a33533b15b8abccf8c0b7cded7397ee855c
M S I <M S I@MSI>
2025-10-23 16:34:58 +0700

    Menampilkan teks copyright.

--- app/src/main/java/com/example/pertemua4/Pertemua4.kt
+++ app/src/main/java/com/example/pertemua4/Pertemua4.kt
@@ -94,3 +94,12 @@ fun AktivitasPertama(modifier: Modifier = Modifier) {
         Box(
             modifier = Modifier.fillMaxSize()
         ) {
+            Text(
+                text = stringResource(id = R.string.copy),
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 50.dp)
+            )
+        }
+    }
+}
