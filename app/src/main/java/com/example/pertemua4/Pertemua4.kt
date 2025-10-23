1cc7d9f1e0fb53f5a95c26de0bfdf8e163ec398b
M S I <M S I@MSI>
2025-10-23 16:33:52 +0700

    Menampilkan nama

--- app/src/main/java/com/example/pertemua4/Pertemua4.kt
+++ app/src/main/java/com/example/pertemua4/Pertemua4.kt
@@ -71,3 +71,12 @@ fun AktivitasPertama(modifier: Modifier = Modifier) {
                 )
 
                 Spacer(modifier = Modifier.width(30.dp))
+
+                Column {
+                    Text(
+                        text = stringResource(id = R.string.nama),
+                        fontSize = 30.sp,
+                        fontFamily = FontFamily.Cursive,
+                        color = Color.White,
+                        modifier = Modifier.padding(top = 25.dp)
+                    )
